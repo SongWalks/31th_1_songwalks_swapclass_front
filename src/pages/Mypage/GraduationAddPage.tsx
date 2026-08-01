56df148bafbcc2c235f73e7eae5bed5b55aae035
Yuna-K8 <kyn824@sookmyung.ac.kr>
2026-08-02 04:25:02 +0900

    feat: 코드 최종 수정

--- src/pages/Mypage/GraduationAddPage.tsx
+++ src/pages/Mypage/GraduationAddPage.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { Icon } from '@iconify/react';
 import Header from '@/components/layout/Header';
